b28d885ae72a062f057fa96251f065765120e494
t0mdd <tomdd@protonmail.com>
2022-05-28 16:06:06 +0100

    Add game function and error handling
    Added a game function that prompts the user for input for 5 rounds of
    rock paper scissors. Introduced error handling for invalid
    inputs. Refactored previous functions to return values that can be
    used as inputs to other functions.

--- rps.js
+++ rps.js
@@ -1,8 +1,8 @@
 const moves = ["Rock","Paper","Scissors"];
 
-const outcomes = {win: "WINNER WINNER CHICKEN DINNER",
-                  draw: "ITS A DRAW NOT GOOD NOT BAD EVERYONES A WINNER AND NOONES A WINNER IT IS WHAT IT IS",
-                  lose: "YOU LOSE. gOOd dAY SIR"};
+const outcomes = {win: "YOU WON THIS ROUND",
+                  draw: "THIS ROUND WAS A DRAW",
+                  lose: "YOU LOST DIS ROUND"};
 
 function random(n){
   return Math.floor(Math.random()*n);
@@ -13,8 +13,11 @@ function moveCode(move){
     case "rock": return 0;
     case "paper": return 1;
     case "scissors": return 2;
-    default: console.error("Invalid move. Type either Rock, Paper, or Scissors");
   }
+}
+
+function moveIsValid(move){
+  return moves.map(m=>m.toLowerCase()).includes(move.toLowerCase());
 }
 
 function computerPlay(){
@@ -24,7 +27,52 @@ function computerPlay(){
 function playSingleRound(playerSelection, computerSelection){
   playerSelectionCode = moveCode(playerSelection);
   computerSelectionCode = moveCode(computerSelection);
-  return (playerSelectionCode == computerSelectionCode) ? outcomes.draw :
-    ((computerSelectionCode + 1)%3 == playerSelectionCode) ? outcomes.win :
-    outcomes.lose;
+  return (playerSelectionCode == computerSelectionCode) ? "draw" :
+    ((computerSelectionCode + 1)%3 == playerSelectionCode) ? "win" :
+    "lose";
+}
+
+function game(){
+  let playerScore = 0;
+  let computerScore = 0;
+  for(let i=0; i < 5; i++){
+    let playerMove;
+    let validMoveChosen = false;
+    while(!validMoveChosen){
+      playerMove = prompt("ENTER YOUR NEXT MOVE: ");
+      if(moveIsValid(playerMove)){
+        validMoveChosen = true;
+      }
+      else{
+        console.error("Invalid move. Choose either Rock, Paper, or Scissors");
+      }
+    }
+    switch(playSingleRound(playerMove, computerPlay())){
+      case "win":
+      console.log(outcomes.win);
+      playerScore++;
+      break;
+      
+      case "draw":
+      console.log(outcomes.draw);
+      break;
+
+      case "lose":
+      console.log(outcomes.lose);
+      computerScore++;
+      break;
+      
+      default:
+      console.error("Invalid return from playSingleRound");
+    }
+  }
+  if(playerScore > computerScore){
+    console.log("WINNER WINNER CHICKEN DINNER YOU WON DA GAME");
+  }
+  else if(playerScore == computerScore){
+    console.log("ITS A DRAW NOT GOOD NOT BAD EVERYONES A WINNER AND NOONES A WINNER IT IS WHAT IT IS");
+  }
+  else{
+    console.log("YOU LOSE. gOOd dAY SIR");
+  }
 }
