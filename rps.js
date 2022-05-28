3db9a99eb251ddbd1d80b4199496304743c46654
t0mdd <tomdd@protonmail.com>
2022-05-28 15:39:44 +0100

    Add function to .js file that plays a single round
    The possible moves are stored in an array such that for any value of i
    in {0,1,2}, moves[(i+1) mod 3] beats moves[i]. This allowed for a much
    clearer and concise implementation of the playSingleRound function
    than my first idea of using nested switch statements operating on each
    possible pair of moves.

--- rps.js
+++ rps.js
@@ -1,9 +1,30 @@
-const outcomes = ["RACK","PAYPA","SIZZA"];
+const moves = ["Rock","Paper","Scissors"];
+
+const outcomes = {win: "WINNER WINNER CHICKEN DINNER",
+                  draw: "ITS A DRAW NOT GOOD NOT BAD EVERYONES A WINNER AND NOONES A WINNER IT IS WHAT IT IS",
+                  lose: "YOU LOSE. gOOd dAY SIR"};
 
 function random(n){
   return Math.floor(Math.random()*n);
 }
 
+function moveCode(move){
+  switch(move.toLowerCase()){
+    case "rock": return 0;
+    case "paper": return 1;
+    case "scissors": return 2;
+    default: console.error("Invalid move. Type either Rock, Paper, or Scissors");
+  }
+}
+
 function computerPlay(){
-  return outcomes[random(3)];
+  return moves[random(3)];
+}
+
+function playSingleRound(playerSelection, computerSelection){
+  playerSelectionCode = moveCode(playerSelection);
+  computerSelectionCode = moveCode(computerSelection);
+  return (playerSelectionCode == computerSelectionCode) ? outcomes.draw :
+    ((computerSelectionCode + 1)%3 == playerSelectionCode) ? outcomes.win :
+    outcomes.lose;
 }
